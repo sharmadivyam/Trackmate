ad4999b6840e5b838df0c959842e7594d955a0e8
Divyam Sharma <ds906@snu.edu.in>
2025-07-07 18:03:51 +0530

    feat: added reporting_manager field and updated signup and login flow

--- taskapp/admin.py
+++ taskapp/admin.py
@@ -1,6 +1,40 @@
 from django.contrib import admin
 from .models import Task
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
 admin.site.register(Task)
+
+
+
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = ('username', 'email', 'is_superuser', 'reporting_manager')
+    list_filter = ('is_superuser', 'reporting_manager')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ( 'email', 'first_name', 'last_name', 'reporting_manager')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Use built-in password1/password2 fields only for admin "Add User" page
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email',  'password1', 'password2', 'reporting_manager'),
+        }),
+    )
+
+    search_fields = ('username', 'email', 'reporting_manager__username')
+    ordering = ('username',)
+
 
 # Register your models here.
--- taskapp/views.py
+++ taskapp/views.py
@@ -14,21 +14,16 @@ import requests
 
 # Create your views here.
 class SignupView(APIView):
-    
-    def get(self, request):
-            return render(request, "taskapp/signup.html")
         
     def post(self,request):
         serializer= UserSerializer(data =request.data, context ={"mode":"signup"})
         if serializer.is_valid():
             serializer.save()
-            return redirect('/tasks/login/')
+            return Response({"status":"200","message":"Signup Succsessful"})
             
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 class LoginView(APIView):
-     def get(self,request):
-         return render(request,"taskapp/login.html")
      def post(self,request):
         serializer= UserSerializer(data =request.data, context ={"mode":"login"})
         if serializer.is_valid():
@@ -39,13 +34,33 @@ class LoginView(APIView):
 
             request.session['access_token'] = access_token
             request.session['username'] = user.email
-            return redirect ('dashboard')
-        return render(request,'login.html', {'error':"invalid credentials"})
+
+            print("Login Successful")
+
+            response = Response({
+                "status": 202,
+                "message": "Login Successful",
+                "access_token": access_token,
+                "refresh_token": str(refresh)
+            }, status=status.HTTP_202_ACCEPTED)
+
+            # Adding token to headers for use in frontend JS or Postman)
+            response['Authorization'] = f'Bearer {access_token}'
+
+            return response
+        
+        print("Serializer Errors:", serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
 
 class DashboardView(APIView):
-    def get(self, request):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get_header(self,request):
         username = request.session.get('username')
         token = request.session.get('access_token')
+
+
         if not token:
             return redirect('login')
             
@@ -53,7 +68,15 @@ class DashboardView(APIView):
         headers ={
             "Authorization" : f"Bearer {token}"
         }
+        return token, headers
+
+
+    def get(self, request):
+        import pdb;pdb.set_trace()
+        username = request.session.get('username')
+        token, headers = self.get_header(request)
         try:
+            # Task.objects.filter(user)
             response = requests.get("http://127.0.0.1:8000/tasks/api/tasks/", headers=headers)
             print(f"Response status: {response.status_code}")  # Debug print
             print(f"Response content: {response.content}")     # Debug print
@@ -74,17 +97,46 @@ class DashboardView(APIView):
             'tasks': tasks
         }) 
     
+    def post(self,request):
+        token, headers = self.get_header(request)
+        data = {
+        "task": request.POST.get("task"),
+        "details": request.POST.get("details"),
+        "start_date": request.POST.get("start_date"),
+        "deadline": request.POST.get("deadline"),
+        "progress": request.POST.get("progress"),
+        "category": request.POST.get("category")
+    }
+
+        response = requests.post("http://127.0.0.1:8000/tasks/api/tasks/", headers=headers, data=data)
+
+        if response.status_code == 201:
+            return redirect('dashboard')
+        else:
+            return self.get(request) 
+
+
+
+
+
+    
 class TaskAPIView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self,request):
+        import pdb;pdb.set_trace();
         user= request.user
         tasks = Task.objects.filter(userid=user)
         serializer = TaskSerializer(tasks,many=True)
         return Response(serializer.data)
     
-
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
    
 
 
